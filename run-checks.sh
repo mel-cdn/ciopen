f4e7339287642ba5ff238bbb97b6b3a7266de368
Mel Cadano <mel.cdn@outlook.ph>
2026-03-01 03:13:28 +0800

    chore: setup unit tests with coverage and update scripts

--- run-checks.sh
+++ run-checks.sh
@@ -8,5 +8,9 @@ pipenv install --dev
 echo "> Installing pre-commit..."
 pre-commit install
 pre-commit install --hook-type commit-msg
+pre-commit run --all-files
+
+echo "> Running tests..."
+./run-tests.sh
 
 echo "> Done!"
---
+++ run-tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+THRESHOLD=95
+
+export PYTHONPATH=src
+export DISABLE_CACHE=True
+
+echo "> Running unit tests with coverage threshold: $THRESHOLD%"
+
+python -m pytest \
+  --cov-config=.coveragerc \
+  --cov=ciopen \
+  --no-cov-on-fail \
+  --cov-fail-under=$THRESHOLD \
+  --cov-branch \
+  --cov-report=term \
+  --cov-report=html:tmp/htmlcov \
+  --cov-report=xml:tmp/coverage.xml \
+  --junitxml=tmp/junit/junit.xml \
+  tests/unit
+
+echo "> Done!"
